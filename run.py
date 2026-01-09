c789801fb1e1195c18c879f6682215738ca612e5
AviVAvi <asmitbohra@gmail.com>
2026-01-09 19:03:51 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from app.main import run_calc
+import sys
 
 if __name__ == "__main__":
-    run_calc()
+    args = sys.argv
+
+    if len(args) == 4:
+        a = int(args[1])
+        b = int(args[2])
+        op = args[3]
+        run_calc(a, b, op)
+    else:
+        run_calc()
