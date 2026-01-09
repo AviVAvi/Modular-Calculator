c54dfc0332f224a5905e0ad733b4400c509bf37e
AviVAvi <asmitbohra@gmail.com>
2026-01-09 19:04:50 +0530

    Update exceptions.py

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,2 +1,7 @@
 class InvalidOperationError(Exception):
+    """Raised when an unsupported operation is selected."""
+    pass
+
+class NegativeNumberError(Exception):
+    """Raised when a negative number is entered."""
     pass
