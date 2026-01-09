c9e0c94782b1b31c86181080e6b994e0156f4743
AviVAvi <asmitbohra@gmail.com>
2026-01-09 19:04:31 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,28 +1,51 @@
 from app.config.settings import NAME, VERSION, DEBUG
 from app.utils.math_ops import add, subtract, multiply, divide, power
 from app.utils.file_ops import save
-from app.exceptions import InvalidOperationError
+from app.exceptions import InvalidOperationError, NegativeNumberError
+import logging
 
-
-def run_calc():
+logging.basicConfig(level=logging.INFO)
+def run_calc(a=None, b=None, op=None):
+    logging.info("App Started")
     print(f"App Name: {NAME}, Version: {VERSION}")
-    a=int(input("Enter number 1: "))
-    b=int(input("Enter number 2: "))
-    op=input("Enter operation(+, -, *, /, **): ")
+    try:
+        a=int(input("Enter number 1: "))
+        b=int(input("Enter number 2: "))
 
-    if op == "+":
-        result = add(a, b)
-    elif op == "-":
-        result = subtract(a, b)
-    elif op == "*":
-        result = multiply(a, b)
-    elif op == "/":
-        result = divide(a, b)
-    elif op == "**":
-        result = power(a, b)
-    else:
-        raise InvalidOperationError("Operation not supported")
+        if a<0 or b<0:
+            raise NegativeNumberError("No negative numbers")
+        else:
+            pass
+        op=input("Enter operation(+, -, *, /, **): ")
+        logging.info(f"Operation {op} was chosen")
+
+        if op == "+":
+            result = add(a, b)
+        elif op == "-":
+            result = subtract(a, b)
+        elif op == "*":
+            result = multiply(a, b)
+        elif op == "/":
+            result = divide(a, b)
+        elif op == "**":
+            result = power(a, b)
+        else:
+            raise InvalidOperationError("Operation not supported")
 
 
-    print("Result:", result)
-    save(result)
+        print("Result:", result)
+        save(result)
+        logging.info("Calculation successful")
+
+    except ValueError:
+        logging.warning("Invalid number input")
+        print("Please enter valid numbers.")
+
+    except InvalidOperationError as e:
+        logging.warning(str(e))
+        print("Error:", e)
+
+    except NegativeNumberError as e:
+        logging.warning(str(e))
+        print("Error:", e)
+
